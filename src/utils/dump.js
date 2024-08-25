4d9fbe40d6c7bf1b67e941d4df2ed7a545ad24ab
raz <vicevasquez95@gmail.com>
2024-08-25 11:00:25 -0400

    rename some files

--- src/utils/dump.js
+++ src/utils/dump.js
@@ -1,12 +1,12 @@
 import { dirname, join} from "path";
 import { fileURLToPath } from "url";
-import { fetch_data } from "./utils/market.js";
+import { fetch_data } from "./market.js";
 import fs from "fs";
 
 const app = async (item) => {
   try{
     const data = await fetch_data(item);
-    fs.writeFile(join(__dirname, 'data/data.json'), JSON.stringify(data, null, 2), (err) => {
+    fs.writeFile(join(__dirname, '../data/data.json'), JSON.stringify(data, null, 2), (err) => {
       if (err) {
         console.error("Error al escribir en el archivo:", err);
         return;
@@ -20,7 +20,7 @@ const app = async (item) => {
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-fs.readFile(join(__dirname, './data/item_info.json'), 'utf8', (err, data) => {
+fs.readFile(join(__dirname, '../data/item_info.json'), 'utf8', (err, data) => {
   const item = JSON.parse(data);
   app(item);
 })
--- src/utils/item_id.py
+++ src/utils/item_id.py
@@ -3,9 +3,15 @@ import json
 import time
 import os
 
+
 items = {
     "Nightmare Case": "https://steamcommunity.com/market/listings/730/Dreams%20%26%20Nightmares%20Case",
+    "AK-47 Asiimov": "https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Asiimov%20%28Minimal%20Wear%29",
+    "Kilowatt Case": "https://steamcommunity.com/market/listings/730/Kilowatt%20Case",
+    "Revolution Case" : "https://steamcommunity.com/market/listings/730/Revolution%20Case"
 }
+
+name = "Nightmare Case"
 
 path = os.path.abspath(os.path.join('src', 'data'))
 
@@ -46,12 +52,13 @@ def get_item_ids(url):
         page.unroute('**')
         browser.close()
         
-    print(f"Item nameid: {item_nameid[0]}, appid: {appid[0]}, name: {list(items.keys())[0]}")
-    return {"item_nameid": item_nameid[0], "appid": appid[0], "name": list(items.keys())[0]}
+    print(f"Item nameid: {item_nameid[0]}, appid: {appid[0]}, name: {name}")
+    return {"item_nameid": item_nameid[0], "appid": appid[0], "name": name}
+
 
 with open(path + "/item_info.json", "+w") as f:
     while True:
-        item_info = get_item_ids(url=items["Nightmare Case"])
+        item_info = get_item_ids(url=items[name])
         if dict(item_info)['item_nameid'] is None:
             print('Failed to get item_nameid')
         else:
